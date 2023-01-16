806b370663859e29f54c102855bab1783515c5c8
sabaghip <sabaghi.p@gmail.com>
2023-01-16 12:50:31 +0330

    last column led debaugged

--- Arduino/Pong/Pong.ino
+++ Arduino/Pong/Pong.ino
@@ -1,23 +1,23 @@
 #include"project.h"
 
 //ROWS
-#define R1 4
-#define R2 5
-#define R3 6
-#define R4 7
-#define R5 8
-#define R6 9
-#define R7 10
-#define R8 11
+#define C1 4
+#define C2 5
+#define C3 6
+#define C4 7
+#define C5 8
+#define C6 9
+#define C7 10
+#define C8 11
 //COLUMNS
-#define C1 12
-#define C2 13
-#define C3 A0
-#define C4 A1
-#define C5 A2
-#define C6 A3
-#define C7 A4
-#define C8 A5
+#define R1 12
+#define R2 13
+#define R3 A0
+#define R4 A1
+#define R5 A2
+#define R6 A3
+#define R7 A4
+#define R8 A5
 //BUTTONS
 #define B1 2
 #define B2 3
@@ -29,7 +29,7 @@ int upButton = 0;
 int downButton = 0;
 
 Ball ball = Ball();
-int time = 0;
+int time = 1;
 int state1 = 0, state2=0;
 
 
@@ -52,22 +52,22 @@ void setup() {
   pinMode(C7, OUTPUT);
   pinMode(C8, OUTPUT);
 
-  digitalWrite(R1,LOW);
-  digitalWrite(R2,LOW);
-  digitalWrite(R3,LOW);
-  digitalWrite(R4,LOW);
-  digitalWrite(R5,LOW);
-  digitalWrite(R6,LOW);
-  digitalWrite(R7,LOW);
-  digitalWrite(R8,LOW);
-  digitalWrite(C1,HIGH);
-  digitalWrite(C2,HIGH);
-  digitalWrite(C3,HIGH);
-  digitalWrite(C4,HIGH);
-  digitalWrite(C5,HIGH);
-  digitalWrite(C6,HIGH);
-  digitalWrite(C7,HIGH);
-  digitalWrite(C8,HIGH);
+  digitalWrite(R1,HIGH);
+  digitalWrite(R2,HIGH);
+  digitalWrite(R3,HIGH);
+  digitalWrite(R4,HIGH);
+  digitalWrite(R5,HIGH);
+  digitalWrite(R6,HIGH);
+  digitalWrite(R7,HIGH);
+  digitalWrite(R8,HIGH);
+  digitalWrite(C1,LOW);
+  digitalWrite(C2,LOW);
+  digitalWrite(C3,LOW);
+  digitalWrite(C4,LOW);
+  digitalWrite(C5,LOW);
+  digitalWrite(C6,LOW);
+  digitalWrite(C7,LOW);
+  digitalWrite(C8,LOW);
 
   pinMode(B1, INPUT_PULLUP);
   pinMode(B2, INPUT_PULLUP);
@@ -87,31 +87,31 @@ void setup() {
 }
 
 void setRow(int row){
-  if(row==1) digitalWrite(R1,HIGH); else digitalWrite(R1,LOW);
-  if(row==2) digitalWrite(R2,HIGH); else digitalWrite(R2,LOW);
-  if(row==3) digitalWrite(R3,HIGH); else digitalWrite(R3,LOW);
-  if(row==4) digitalWrite(R4,HIGH); else digitalWrite(R4,LOW);
-  if(row==5) digitalWrite(R5,HIGH); else digitalWrite(R5,LOW);
-  if(row==6) digitalWrite(R6,HIGH); else digitalWrite(R6,LOW);
-  if(row==7) digitalWrite(R7,HIGH); else digitalWrite(R7,LOW);
-  if(row==8) digitalWrite(R8,HIGH); else digitalWrite(R8,LOW);
+  if(row==1) digitalWrite(R1,LOW); else digitalWrite(R1,HIGH);
+  if(row==2) digitalWrite(R2,LOW); else digitalWrite(R2,HIGH);
+  if(row==3) digitalWrite(R3,LOW); else digitalWrite(R3,HIGH);
+  if(row==4) digitalWrite(R4,LOW); else digitalWrite(R4,HIGH);
+  if(row==5) digitalWrite(R5,LOW); else digitalWrite(R5,HIGH);
+  if(row==6) digitalWrite(R6,LOW); else digitalWrite(R6,HIGH);
+  if(row==7) digitalWrite(R7,LOW); else digitalWrite(R7,HIGH);
+  if(row==8) digitalWrite(R8,LOW); else digitalWrite(R8,HIGH);
 }
 
 void setLEDInActiveRow(int col, int state){
-  if(col==1) digitalWrite(C1,1 - state);
-  if(col==2) digitalWrite(C2,1 - state);
-  if(col==3) digitalWrite(C3,1 - state);
-  if(col==4) digitalWrite(C4,1 - state);
-  if(col==5) digitalWrite(C5,1 - state);
-  if(col==6) digitalWrite(C6,1 - state);
-  if(col==7) digitalWrite(C7,1 - state);
-  if(col==8) digitalWrite(C8,1 - state);
+  if(col==1) digitalWrite(C1,state);
+  if(col==2) digitalWrite(C2,state);
+  if(col==3) digitalWrite(C3,state);
+  if(col==4) digitalWrite(C4,state);
+  if(col==5) digitalWrite(C5,state);
+  if(col==6) digitalWrite(C6,state);
+  if(col==7) digitalWrite(C7,state);
+  if(col==8) digitalWrite(C8,state);
 }
 
 void updateLED(){
-  for(int i = 0; i < Y_SIZE_OF_LED; i++){
+  for(int i = 0; i <= Y_SIZE_OF_LED; i++){
     setRow(i + 1);
-    for(int j = 0; j < X_SIZE_OF_LED; j++){ 
+    for(int j = 0; j <= X_SIZE_OF_LED; j++){ 
       setLEDInActiveRow(j + 1, array[j][i]);
     }
     delay(1);    
@@ -156,7 +156,7 @@ void checkLeftController(){
 
 void loop() {
   checkLeftController();
-  if(time%70 == 0)
+  if(time%45 == 0)
     ball.move();
   updateLED();
   time++;
